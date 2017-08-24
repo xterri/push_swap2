6c0fdf7b9abe6ad8aa1b1d6b9441f78961e72587
terri <thuynh1989@gmail.com>
2017-08-23 23:30:59 -0700

    changed swap to one function, edited swap_both to use swap function

--- srcs/swap.c
+++ srcs/swap.c
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap_both.c                                        :+:      :+:    :+:   */
+/*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/22 22:52:52 by thuynh            #+#    #+#             */
-/*   Updated: 2017/08/23 23:09:41 by thuynh           ###   ########.fr       */
+/*   Created: 2017/08/22 20:23:03 by thuynh            #+#    #+#             */
+/*   Updated: 2017/08/23 23:09:02 by thuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap_both(t_list *list)
+void	swap(int *arr)
 {
-	swap(list->stk_a);
-	swap(list->stk_b);
-}
+	if (!arr)
+		return ;
+	ft_swap(&arr[0], &arr[1]);
+}	
