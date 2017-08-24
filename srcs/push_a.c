85ff6bcfa79646b5d1a4129d8de1b586c40b1aba
terri <thuynh1989@gmail.com>
2017-08-24 11:24:34 -0700

    minor issues with push functions, fixed

--- srcs/push_a.c
+++ srcs/push_a.c
@@ -6,7 +6,7 @@
 /*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/23 20:01:58 by thuynh            #+#    #+#             */
-/*   Updated: 2017/08/23 22:19:34 by thuynh           ###   ########.fr       */
+/*   Updated: 2017/08/24 11:12:06 by thuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	push_a(t_list *list)
 {
-	if ((list->b_ind < 0) || !list->stk_b)
-		return ;
+	if ((list->b_ind <= 0) || !list->stk_b)
+		return ;	
 	ft_pop(list->stk_b, &list->tmp, &list->b_ind);
 	ft_push(list->stk_a, list->tmp, &list->a_ind);
 }
