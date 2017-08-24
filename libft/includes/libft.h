1c6bf4026a8de34ad25a5db53afbe1fb7005386a
terri <thuynh1989@gmail.com>
2017-08-23 23:28:49 -0700

    created pop and push functions for libft

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/21 18:56:46 by thuynh            #+#    #+#             */
-/*   Updated: 2017/08/23 17:26:38 by thuynh           ###   ########.fr       */
+/*   Updated: 2017/08/23 20:46:51 by thuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ void	ft_bzero(void *s, size_t n);
 int		ft_isdigit(int c);
 void	*ft_memalloc(size_t size);
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_pop(int *arr, int *tmp, int *index);
+void	ft_push(int *arr, int val, int *index);
 int		ft_strcmp(const char *s1, const char *s2);
 void	ft_swap(int *a, int *b);
 
