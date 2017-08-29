f8da8893fd4fab174bde9908524dbab3589b2fa9
terri <thuynh1989@gmail.com>
2017-08-28 17:16:28 -0700

    fixed mem leak

--- srcs/get_oper.c
+++ srcs/get_oper.c
@@ -6,7 +6,7 @@
 /*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/23 18:36:26 by thuynh            #+#    #+#             */
-/*   Updated: 2017/08/26 20:00:36 by thuynh           ###   ########.fr       */
+/*   Updated: 2017/08/28 17:12:13 by thuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 int		get_oper(t_list *list)
 {
-	int		ret;
+	int		count;
 	int		op_nb;
 	char	*str;
 
+	count = 0;
 	for (int i = 0; list->a_ind > list->b_ind ? i <= list->a_ind : i <= list->b_ind; i++)
 		printf("%d\t\t%d\n", list->stk_a[i], list->stk_b[i]);
 	puts("");
-	while ((ret = get_next_line(0, &str)))
+	while (get_next_line(0, &str) > 0)
 	{
-		if (!ft_strlen(str))
+		if (!ft_strlen(str) )
 			break ;
 		!(op_nb = check_oper(str)) ? error(5, list) : 0;
 		do_oper(list, op_nb);
@@ -31,9 +32,9 @@ int		get_oper(t_list *list)
 		for (int i = 0; list->a_ind > list->b_ind ? i <= list->a_ind : i <= list->b_ind; i++)
 			printf("%d\t\t%d\n", list->stk_a[i], list->stk_b[i]);
 		puts("");
-		free(str);
-		str = NULL;
+		ft_strdel(&str);
+		count++;
 	}
-	free(str);
+	count && str ? ft_strdel(&str) : 0;
 	return (1);
 }
